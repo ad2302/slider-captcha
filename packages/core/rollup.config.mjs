f8d55ebfe195f3bc3b5e2362eba8f4b1d2f47068
bung87 <crc32@qq.com>
2022-05-08 16:11:51 +0800

    package

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -2,15 +2,21 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from '@rollup/plugin-typescript';
-import postcss from "rollup-plugin-postcss";
 import json from '@rollup/plugin-json';
-import packageJson from "./package.json";
-import image from '@rollup/plugin-image';
+import { readFile } from "fs/promises";
+const packageJson = JSON.parse(await readFile("./package.json"));
 import path from 'path';
 import { babel } from '@rollup/plugin-babel';
+import native from 'rollup-plugin-native';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 export default {
   input: "src/index.ts",
+  external:['sharp','nanoid'],
   output: [
     {
       file: packageJson.main,
@@ -26,9 +32,12 @@ export default {
   plugins: [
     json(),
     peerDepsExternal(),
+    native({
+        // platformName: "${dirname}/precompiled/${nodePlatform}-${nodeArchitecture}/node.napi.node",
+        //platformName: "${dirname}/${basename}-${nativePlatform}-${nativeArchitecture}.node",
+        }),
     resolve(),
     typescript({
-      "target": 'es2015',
       declaration: true,
       declarationDir: 'typing',
       // "typeRoots": [path.join(__dirname,'types')],
@@ -36,11 +45,5 @@ export default {
     }),
     commonjs(),
     babel({ babelHelpers: 'bundled' }),
-    postcss({
-      extract: false,
-      modules: false,
-      use: ['sass'],
-    }),
-    image(),
   ]
 };
